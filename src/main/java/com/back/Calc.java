cb11021e6ea56ba57000e8247962789ff3c5e9b6
heeun98 <pygphu98@gmail.com>
2025-05-30 14:40:31 +0900

    abc

---
+++ src/main/java/com/back/Calc.java
@@ -0,0 +1,23 @@
+package com.back;
+
+import java.util.Arrays;
+
+public class Calc {
+
+    public static String[] plusSplit;
+
+    public static int run(String s) {
+
+
+        String[] bits = s.split(" ");
+
+        int result = Integer.parseInt(bits[0]);
+
+        for (int i = 0; i < bits.length - 1; i++) {
+
+
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/back/RqTest.java
@@ -0,0 +1,36 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RqTest {
+
+    @Test
+    @DisplayName("rq.getActionName()")
+    void t1() {
+
+        Rq rq = new Rq("수정?id=1");
+
+        String actionName = rq.getActionName();
+
+        assertEquals("수정", actionName);
+    }
+
+
+    @Test
+    @DisplayName("rq.getActionName()")
+    void t2() {
+
+        Rq rq = new Rq("삭제?id=1");
+
+        String actionName = rq.getActionName();
+
+        assertEquals("삭제", actionName);
+    }
+
+
+
+
+}
