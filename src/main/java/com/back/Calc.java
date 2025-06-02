9233f99ce9d24d52e102bcbbb85f4bfd1f013487
shihan00321 <shihan001@naver.com>
2025-06-02 17:25:41 +0900

    TDD Test1~11 성공 케이스 구현

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,9 +3,19 @@ package com.back;
 public class Calc {
     public static int run(String input) {
         String[] split = input.split(" ");
-        return switch (split[1]) {
-            case "+" -> Integer.parseInt(split[0]) + Integer.parseInt(split[2]);
-            case "-" -> Integer.parseInt(split[0]) - Integer.parseInt(split[2]);
+        int sum = Integer.parseInt(split[0]);
+        int curIndex = 1;
+        while (curIndex < split.length) {
+            sum = operation(sum, Integer.parseInt(split[curIndex + 1]), split[curIndex]);
+            curIndex += 2;
+        }
+        return sum;
+    }
+
+    public static int operation(int sum, int num, String operator) {
+        return switch (operator) {
+            case "+" -> sum + num;
+            case "-" -> sum - num;
             default -> 0;
         };
     }
