f7e8ab2449338d15a08f08f2968f80bf31459840
shihan00321 <shihan001@naver.com>
2025-06-02 17:21:31 +0900

    TDD Test1~7 성공 케이스 구현

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,6 +3,10 @@ package com.back;
 public class Calc {
     public static int run(String input) {
         String[] split = input.split(" ");
-        return Integer.parseInt(split[0]) + Integer.parseInt(split[2]);
+        return switch (split[1]) {
+            case "+" -> Integer.parseInt(split[0]) + Integer.parseInt(split[2]);
+            case "-" -> Integer.parseInt(split[0]) - Integer.parseInt(split[2]);
+            default -> 0;
+        };
     }
 }
